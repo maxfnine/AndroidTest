5c330e099c5ade4c128a0198c932530b9fc00a31
maxfnine <maxfnine@gmail.com>
2019-05-01 21:46:09 +0300

    added a text field

--- app/src/main/java/com/example/androidtest/MainActivity.kt
+++ app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var someOtherValue="valueB"
+        var value="valueA"
     }
 }
